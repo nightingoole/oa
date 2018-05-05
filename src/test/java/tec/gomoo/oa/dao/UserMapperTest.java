1fa1dc069a5ff89c5e65d1eafe796a901f6fe1c8
phw-nightingale <937855602@qq.com>
2018-05-05 12:31:46 +0800

    完善框架

--- src/test/java/tec/gomoo/oa/dao/UserMapperTest.java
+++ src/test/java/tec/gomoo/oa/dao/UserMapperTest.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import tec.gomoo.oa.OaApplicationTests;
 import tec.gomoo.oa.model.User;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class UserMapperTest extends OaApplicationTests {
@@ -16,6 +20,47 @@ public class UserMapperTest extends OaApplicationTests {
     public void selectByPrimaryKey() {
         User user = userMapper.selectByPrimaryKey(1);
         System.out.println(user == null ? "null" : user.toString());
+    }
+
+    @Test
+    public void insertSelective() {
+        User user = new User();
+        user.setUsername("nightingale");
+        user.setPassword("199798");
+        user.setPhone("1897288714");
+        user.setEmail("997855021@qq.com");
+        user.setCreateUser("admin");
+        user.setCreateUserId(1);
+        userMapper.insertSelective(user);
+    }
+
+    @Test
+    public void updateByPrimaryKeySelective() {
+        User user = new User();
+        user.setId(5);
+        user.setEmail("phw.nightingale@gmail.com");
+        userMapper.updateByPrimaryKeySelective(user);
+    }
+
+    @Test
+    public void deleteByPrimaryKey() {
+        User user = new User();
+        user.setId(6);
+        userMapper.deleteByPrimaryKey(user.getId());
+    }
+
+    @Test
+    public void selectByConditions() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", "phw");
+        List<User> users = userMapper.selectByConditions(map);
+        System.out.println(users);
+    }
+
+    @Test
+    public void selectAll() {
+        List<User> users = userMapper.selectAll();
+        System.out.println(users);
     }
 
 }
